4d42e0be8d5c096531f3251301495bb6736248be
Artem Novikov <novartole@gmail.com>
2025-03-07 14:57:10 +0300

    feat | affp29 : impl props and ops for Edge

--- crates/sal-3dlib-core/src/topology/shape/edge.rs
+++ crates/sal-3dlib-core/src/topology/shape/edge.rs
@@ -1,4 +1,9 @@
-use crate::props::Attributes;
+use super::vertex::Vertex;
+use crate::{
+    gmath::Vector,
+    ops::transform::{Rotate, Translate},
+    props::{Attributes, Center, Length},
+};
 ///
 /// One-dimensional shape corresponding to a curve.
 ///
@@ -7,8 +12,8 @@ use crate::props::Attributes;
 /// - the inner implementation specific to the kernel - `E`,
 /// - an optional attribute.
 pub struct Edge<const N: usize, E, T> {
-    inner: E,
-    attrs: Option<Attributes<T>>,
+    pub(crate) inner: E,
+    pub(crate) attrs: Option<Attributes<T>>,
 }
 //
 //
@@ -33,6 +38,79 @@ where
         Self {
             inner: self.inner.clone(),
             attrs: self.attrs.clone(),
+        }
+    }
+}
+//
+//
+impl<const N: usize, E, V, T> Center for Edge<N, E, T>
+where
+    E: Center<Output = V>,
+{
+    type Output = Vertex<N, V, T>;
+    //
+    //
+    fn center(&self) -> Self::Output {
+        Vertex::<N, V, T> {
+            inner: self.inner.center(),
+            attrs: None,
+        }
+    }
+}
+///
+/// Edge with direction.
+pub trait Direction<Dir> {
+    ///
+    /// Returns the direction.
+    fn dir(&self) -> Dir;
+}
+//
+//
+impl<const N: usize, E, T, V> Direction<V> for Edge<N, E, T>
+where
+    E: Direction<V>,
+    V: Into<Vector<N>>,
+{
+    fn dir(&self) -> V {
+        self.inner.dir()
+    }
+}
+//
+//
+impl<const N: usize, E, T> Length for Edge<N, E, T>
+where
+    E: Length,
+{
+    fn len(&self) -> f64 {
+        self.inner.len()
+    }
+}
+//
+//
+impl<const N: usize, E, V, A, T> Rotate<Vertex<N, V, T>, A> for Edge<N, E, T>
+where
+    E: Rotate<V, A>,
+    A: Into<Vector<N>>,
+{
+    fn rotate(self, origin: Vertex<N, V, T>, axis: A, angle: f64) -> Self {
+        let origin = origin.inner;
+        Self {
+            inner: self.inner.rotate(origin, axis, angle),
+            attrs: self.attrs,
+        }
+    }
+}
+//
+//
+impl<const N: usize, E, D, T> Translate<D> for Edge<N, E, T>
+where
+    E: Translate<D>,
+    D: Into<Vector<N>>,
+{
+    fn translate(self, dir: D) -> Self {
+        Self {
+            inner: self.inner.translate(dir),
+            attrs: self.attrs,
         }
     }
 }
