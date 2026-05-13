5a79602aae554da4334ec0ce4a146563e71e22cc
KnstAnt <knstantna@gmail.com>
2026-05-13 11:40:32 +0300

    udpate stl reading, fix test

--- crates/sal-3dlib/src/tests/file_io.rs
+++ crates/sal-3dlib/src/tests/file_io.rs
@@ -4,12 +4,17 @@ use sal_core::dbg::Dbg;
 use std::time::Instant;
 use std::{path::Path, sync::Arc};
 
-
+#[ignore]
 #[test]
 fn load_large_stl() {
     let path = "src/tests/assets/SURF_Sofiya.stl";
     let time = Instant::now();
-    let mesh = load_stl(Path::new(path), 1000.).unwrap();
-    let elapsed = time.elapsed();
-    println!("time:{:?} vertices:{}", elapsed, mesh.vertices().len(),);
+    let mesh_no_scale = load_stl(Path::new(path), 1.).unwrap();
+    let elapsed_no_scale = time.elapsed();
+    let time = Instant::now();
+    let mesh_scale = load_stl(Path::new(path), 1000.).unwrap();
+    let elapsed_scale = time.elapsed();    
+    println!("no_scale, time:{:?} vertices:{};\n scale, time:{:?} vertices:{}",
+    elapsed_no_scale, mesh_no_scale.vertices().len(),
+    elapsed_scale, mesh_scale.vertices().len(),);
 }
