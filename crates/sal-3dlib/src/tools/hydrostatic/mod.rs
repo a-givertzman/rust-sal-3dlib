ae890406c67f6173ff1cc6ac49de1b2812d6dda0
KnstAnt <knstantna@gmail.com>
2026-05-14 15:12:44 +0300

    fix center of volume

--- crates/sal-3dlib/src/tools/hydrostatic/mod.rs
+++ crates/sal-3dlib/src/tools/hydrostatic/mod.rs
@@ -17,8 +17,9 @@ pub fn calculate_hydrostatic(mesh: &TriMesh, center: Vec3, heel: f64, trim: f64,
     let plane = Plane::from_point_and_normal(local_point, local_normal);
     let sliced_mesh = plane.slice_mesh(mesh);
     let hydrostatics = sliced_mesh.hydrostatics();
-    let center_of_buoyancy = hydrostatics.center_of_buoyancy;
-    (hydrostatics.volume, center_of_buoyancy)
+    let center = hydrostatics.center_of_buoyancy;
+    let center = Vec3::new(center.x, -center.y, center.z);
+    (hydrostatics.volume, center)
 }
 
 pub fn calculate_waterline(mesh: &TriMesh, center: Vec3, heel: f64, trim: f64, draught: f64) -> (f64, Vec3) {
@@ -33,8 +34,9 @@ pub fn calculate_waterline(mesh: &TriMesh, center: Vec3, heel: f64, trim: f64, d
     sliced_mesh.waterline_edges = sliced_mesh.waterline_edges.iter()
        .map(|v| [isometry.transform_point(v[0]), isometry.transform_point(v[1])] ).collect();
     let (area, area_center) = sliced_mesh.calculate_waterline_properties();
-
-    (area, isometry.inverse_transform_point(area_center))
+    let center = isometry.inverse_transform_point(area_center);
+    let center = Vec3::new(center.x, -center.y, center.z);
+    (area, center)
 }
 
 pub fn calculate_inertia(mesh: &TriMesh, center: Vec3, heel: f64, trim: f64, draught: f64) -> (f64, f64) {
